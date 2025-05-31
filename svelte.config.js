f48d5bf540edb1b5891682efa5b3389b6d8f5d59
Sophie Dové <sophie@sophies-mbp.zhaw.ch>
2025-05-31 16:46:50 +0200

    Add Netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,11 @@
+import adapter from '@sveltejs/adapter-netlify';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
 const config = {
+  kit: {
+    adapter: adapter()
+  },
+  preprocess: vitePreprocess(),
   compilerOptions: {
     runes: true
   }
